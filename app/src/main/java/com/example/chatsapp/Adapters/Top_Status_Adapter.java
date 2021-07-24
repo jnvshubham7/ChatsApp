720087e9398c4a96e698db6230e41f502c4d215f
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2021-07-24 17:47:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chatsapp/Adapters/Top_Status_Adapter.java
@@ -0,0 +1,96 @@
+package com.example.chatsapp.Adapters;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.chatsapp.Activities.Main_Activity;
+import com.example.chatsapp.Models.Status;
+import com.example.chatsapp.Models.User_Status;
+import com.example.chatsapp.R;
+import com.example.chatsapp.databinding.ItemStatusBinding;
+
+import java.util.ArrayList;
+
+import omari.hamza.storyview.StoryView;
+import omari.hamza.storyview.callback.StoryClickListeners;
+import omari.hamza.storyview.model.MyStory;
+
+public class Top_Status_Adapter extends RecyclerView.Adapter<Top_Status_Adapter.TopStatusViewHolder> {
+
+    Context context;
+    ArrayList<User_Status> userStatuses;
+
+    public Top_Status_Adapter(Context context, ArrayList<User_Status> userStatuses) {
+        this.context = context;
+        this.userStatuses = userStatuses;
+    }
+
+    @NonNull
+    @Override
+    public TopStatusViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_status, parent, false);
+        return new TopStatusViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TopStatusViewHolder holder, int position) {
+
+        User_Status userStatus = userStatuses.get(position);
+
+        Status lastStatus = userStatus.getStatuses().get(userStatus.getStatuses().size() - 1);
+
+        Glide.with(context).load(lastStatus.getImageUrl()).into(holder.binding.image);
+
+        holder.binding.circularStatusView.setPortionsCount(userStatus.getStatuses().size());
+
+        holder.binding.circularStatusView.setOnClickListener(v -> {
+            ArrayList<MyStory> myStories = new ArrayList<>();
+            for(Status status : userStatus.getStatuses()) {
+                myStories.add(new MyStory(status.getImageUrl()));
+            }
+
+            new StoryView.Builder(((Main_Activity)context).getSupportFragmentManager())
+                    .setStoriesList(myStories) // Required
+                    .setStoryDuration(5000) // Default is 2000 Millis (2 Seconds)
+                    .setTitleText(userStatus.getName()) // Default is Hidden
+                    .setSubtitleText("") // Default is Hidden
+                    .setTitleLogoUrl(userStatus.getProfileImage()) // Default is Hidden
+                    .setStoryClickListeners(new StoryClickListeners() {
+                        @Override
+                        public void onDescriptionClickListener(int position1) {
+                            //your action
+                        }
+
+                        @Override
+                        public void onTitleIconClickListener(int position1) {
+                            //your action
+                        }
+                    }) // Optional Listeners
+                    .build() // Must be called before calling show method
+                    .show();
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return userStatuses.size();
+    }
+
+    public static class TopStatusViewHolder extends RecyclerView.ViewHolder {
+
+        ItemStatusBinding binding;
+
+        public TopStatusViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemStatusBinding.bind(itemView);
+        }
+    }
+
+}
