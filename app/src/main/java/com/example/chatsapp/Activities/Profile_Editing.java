ea0fff4ccb97ce7aad6b62b20329b34b3b7edb34
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-05-22 07:20:54 +0530

    chore: Update Profile_Editing activity with EdgeToEdge and WindowInsetsCompat

--- app/src/main/java/com/example/chatsapp/Activities/Profile_Editing.java
+++ app/src/main/java/com/example/chatsapp/Activities/Profile_Editing.java
@@ -1,12 +1,26 @@
-//package com.example.chatsapp.Activities;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.os.Bundle;
-//
-//public class ProfileEditing extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_profile_editing);
+package com.example.chatsapp.Activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chatsapp.R;
+
+public class Profile_Editing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile_editing);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
